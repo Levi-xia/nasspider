5f61eb25bdef6dfdbceeca1689408c667bb43963
Levi <15652612587@163.com>
2025-02-08 00:42:39 +0800

    up

--- main.go
+++ main.go
@@ -19,22 +19,52 @@ func main() {
 	logger.InitLog()
 
 	var err error
-	tvTask := bo.TVTask{
-		ID:           1,
-		URL:          "https://www.ddmp4.cc/html/KisTCQJJJJJQ.html",
-		Type:         "magnet",
-		DownloadPath: "/downloads/",
-		Status:       0,
-		CurrentEp:    18,
-		TotalEp:      38,
-		Provider:     "domp4",
-		Downloader:   "thunder",
+	tvTasks := []bo.TVTask{
+		{
+			ID:           1,
+			Name:         "白色橄榄树",
+			URL:          "https://www.ddmp4.cc/html/KisTCQJJJJJQ.html",
+			Type:         "magnet",
+			DownloadPath: "/downloads/",
+			Status:       0,
+			CurrentEp:    18,
+			TotalEp:      38,
+			Provider:     "domp4",
+			Downloader:   "thunder",
+		},
+		{
+			ID:           2,
+			Name:         "五福临门",
+			URL:          "https://www.ddmp4.cc/html/p0KStZMMMMMZ.html",
+			Type:         "magnet",
+			DownloadPath: "/downloads/",
+			Status:       0,
+			CurrentEp:    23,
+			TotalEp:      24,
+			Provider:     "domp4",
+			Downloader:   "thunder",
+		},
+		{
+			ID:           3,
+			Name:         "无所畏惧-2",
+			URL:          "https://www.ddmp4.cc/html/SI4Qy4222224.html",
+			Type:         "magnet",
+			DownloadPath: "/downloads/",
+			Status:       0,
+			CurrentEp:    22,
+			TotalEp:      40,
+			Provider:     "domp4",
+			Downloader:   "thunder",
+		},
 	}
-	provider := provider.ProviderMap[constants.ProviderName(tvTask.Provider)]
-	downloader := downloader.DownloaderMap[constants.DownloaderName(tvTask.Downloader)]
+	for _, tvTask := range tvTasks {
+		logger.Logger.Infof("开始任务:%s\n", tvTask.Name)
+		provider := provider.ProviderMap[constants.ProviderName(tvTask.Provider)]
+		downloader := downloader.DownloaderMap[constants.DownloaderName(tvTask.Downloader)]
 
-	err = task.DoTask(provider, downloader, tvTask)
-	if err != nil {
-		fmt.Printf("%v", err)
+		err = task.DoTask(provider, downloader, tvTask)
+		if err != nil {
+			fmt.Printf("%v", err)
+		}
 	}
 }
