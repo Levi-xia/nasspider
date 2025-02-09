826547a203c5c4f99adee76278359b7828c7add1
Levi <15652612587@163.com>
2025-02-09 20:56:33 +0800

    up

--- pkg/task/task.go
+++ pkg/task/task.go
@@ -13,6 +13,22 @@ import (
 
 func DoTask(tvTask bo.TVTask, isCron bool) error {
 	logger.Logger.Infof("开始执行任务:%v", tvTask.Name)
+
+	if isCron && tvTask.Status != int(constants.Waiting) {
+		logger.Logger.Infof("任务状态不是等待中, 跳过，等待下次执行")
+		return nil
+	}
+	if tvTask.Status == int(constants.Doing) {
+		logger.Logger.Infof("任务正在追更中, 跳过，等待执行完成")
+		return nil
+	}
+	// 任务结束后更新的状态
+	var upStatus = constants.Waiting
+
+	// 任务修改为追更中
+	if _, err := service.UpdateStatus(&bo.UpdateStatusRequest{ID: tvTask.ID, Status: int(constants.Doing)}); err != nil {
+		return err
+	}
 	var (
 		err       error
 		URLs      []string
@@ -20,18 +36,11 @@ func DoTask(tvTask bo.TVTask, isCron bool) error {
 	)
 	defer func() {
 		if err != nil {
-			service.UpdateStatus(&bo.UpdateStatusRequest{ID: tvTask.ID, Status: int(constants.Error)})
+			upStatus = constants.Error
 			logger.Logger.Errorf("任务失败:%v", err)
 		}
+		service.UpdateStatus(&bo.UpdateStatusRequest{ID: tvTask.ID, Status: int(upStatus)})
 	}()
-	if isCron && tvTask.Status != int(constants.Waiting) {
-		logger.Logger.Infof("任务状态不是等待中, 跳过，等待下次执行")
-		return nil
-	}
-	if tvTask.Status != int(constants.Doing) {
-		logger.Logger.Infof("任务正在追更中, 跳过，等待执行完成")
-	}
-
 	if tvTask.TotalEp != 0 && tvTask.CurrentEp >= tvTask.TotalEp {
 		if _, err := service.UpdateStatus(&bo.UpdateStatusRequest{ID: tvTask.ID, Status: int(constants.Finish)}); err != nil {
 			return err
@@ -44,11 +53,6 @@ func DoTask(tvTask bo.TVTask, isCron bool) error {
 	if p == nil || d == nil {
 		return fmt.Errorf("provider or downloader not found")
 	}
-	// 任务修改为追更中
-	if _, err := service.UpdateStatus(&bo.UpdateStatusRequest{ID: tvTask.ID, Status: int(constants.Doing)}); err!= nil {
-		return err
-	}
-
 	// 开始解析provider
 	if URLs, currentEp, err = p.ParseURLs(tvTask.URL, tvTask.CurrentEp); err != nil {
 		return err
@@ -74,14 +78,10 @@ func DoTask(tvTask bo.TVTask, isCron bool) error {
 	if _, err := service.UpdateCurrentEp(&bo.UpdateCurrentEpRequest{ID: tvTask.ID, CurrentEp: currentEp}); err != nil {
 		return err
 	}
-
 	// 任务执行完成后修改任务状态
-	status := constants.Waiting
+	upStatus = constants.Waiting
 	if currentEp == tvTask.TotalEp {
-		status = constants.Finish	
-	}
-	if _, err := service.UpdateStatus(&bo.UpdateStatusRequest{ID: tvTask.ID, Status: int(status)}); err != nil {
-		return err
+		upStatus = constants.Finish
 	}
 	return nil
 }
